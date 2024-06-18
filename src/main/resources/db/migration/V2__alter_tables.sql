26237e8d5007bd6e3c2a6dc81b3b6c826877c89c
raiym <76080041+raiym2048@users.noreply.github.com>
2024-06-18 15:39:46 +0600

    update sql

--- src/main/resources/db/migration/V2__alter_tables.sql
+++ src/main/resources/db/migration/V2__alter_tables.sql
@@ -1,6 +1,8 @@
 
-alter table if exists medicine add constraint FK6jo8tdud51x538342ecwx9rj5 foreign key (take_time_id) references take_time
-    ;alter table if exists medicine add constraint FKinhaj3lokxnc8fif7r9hkhh1d foreign key (type_id) references medicine_type
-    ;alter table if exists take_time add constraint FKejb6jpwffl1luu8k8x2dtwdft foreign key (day_id) references take_time_day
-    ;alter table if exists users_medicines add constraint FK9pdwbl9lpuhqq9sgf943c420e foreign key (medicines_id) references medicine
-    ;alter table if exists users_medicines add constraint FKp68ec1eb5jckm57f6ltfddn9f foreign key (user_id) references users;
+ alter table if exists medicine add constraint FK6jo8tdud51x538342ecwx9rj5 foreign key (take_time_id) references take_time
+    ; alter table if exists medicine add constraint FKinhaj3lokxnc8fif7r9hkhh1d foreign key (type_id) references medicine_type
+    ; alter table if exists take_time add constraint FKejb6jpwffl1luu8k8x2dtwdft foreign key (day_id) references take_time_day
+    ; alter table if exists users_medicines add constraint FK9pdwbl9lpuhqq9sgf943c420e foreign key (medicines_id) references medicine
+    ; alter table if exists users_medicines add constraint FKp68ec1eb5jckm57f6ltfddn9f foreign key (user_id) references users
+    ; alter table if exists users_taken_medicine add constraint FKi8oetd8gggw0buoxnf7ysrmdl foreign key (taken_medicine_id) references medicine
+    ; alter table if exists users_taken_medicine add constraint FKj5yu2xd8mbdqg7qya6i4d7r7w foreign key (user_id) references users;
